5b1616cfe0588861ac97e8d42de73fbba6ce274a
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-07-08 14:23:40 +0400

    restructured function.js and text-home.js

--- js/text-home.js
+++ js/text-home.js
@@ -1,50 +1,47 @@
-
-	
-	var opac = anime({
-  targets: '.letter',
-	opacity:1,
+var opac1 = anime({
+	targets: '.letter',
+	opacity: 1,
 	delay: function(el, index) {
-	  return index * 1000;
+		return index * 1000;
 	},
 });
 
-var opac = anime({
-  targets: '.letter2',
-	opacity:1,
+var opac2 = anime({
+	targets: '.letter2',
+	opacity: 1,
 	delay: function(el, index) {
-	  return index * 1200;
+		return index * 1200;
 	},
 });
 
-var opac = anime({
-  targets: '.letter3',
-	opacity:1,
+var opac3 = anime({
+	targets: '.letter3',
+	opacity: 1,
 	delay: function(el, index) {
-	  return index * 2300;
+		return index * 2300;
 	},
 });
 
-var opac = anime({
-  targets: '.letter4',
-	opacity:1,
+var opac4 = anime({
+	targets: '.letter4',
+	opacity: 1,
 	delay: function(el, index) {
-	  return index * 1200;
+		return index * 1200;
 	},
 });
 
-var opac = anime({
-  targets: '.letter5',
-	opacity:1,
+var opac5 = anime({
+	targets: '.letter5',
+	opacity: 1,
 	delay: function(el, index) {
-	  return index * 1900;
+		return index * 1900;
 	},
 });
 
-var opac = anime({
-  targets: '.letter6',
-	opacity:1,
+var opac6 = anime({
+	targets: '.letter6',
+	opacity: 1,
 	delay: function(el, index) {
-	  return index * 1700;
+		return index * 1700;
 	},
 });
-
